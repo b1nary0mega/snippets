ac28b4aa7ecb2a8f715afac443117ad8a35d47fb
Jimmi <b1nary0mega@users.noreply.github.com>
2019-09-10 09:45:58 -0400

    Update powershell.ps1
    added snippet to bulk rename files and keep extension

--- powershell.ps1
+++ powershell.ps1
@@ -11,3 +11,6 @@ PS > Get-WmiObject -Query "select * from Win32_Process where name like 'chrome%'
 
 # query to find commandlets that have "write" in their name
 PS > Get-Command | Where {$_.Name -like "*write*"}
+
+# rename all files BASE and keep EXTENSION (-WhatIf == see what would happen; remove to actually perform)
+PS > Get-ChildItem -Path .\ | Rename-Item -NewName {$_.Basename.Replace("old_name","new_name") + $_.extension} -WhatIf -Verbose
